5211b8cf4addd65ae3327883767ccacae4e22de6
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-12-22 22:32:37 +0200

    frontend setup

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,9 @@ const app = express();
 // enables accepting body data that comes through request.body
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 //connect to openai routes
 app.use("openai", require("./routes/openaiRoutes"));
