3022f5846c7bd3747993fb64931c973e0526e404
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-12-22 22:10:51 +0200

    request body

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const port = process.env.PORT || 5000;
 // initialize express
 const app = express();
 
+// Enable body parser (Middleware)
+// enables accepting body data that comes through request.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 //connect to openai routes
-app.use('openai', require('./routes/openaiRoutes'))
+app.use("openai", require("./routes/openaiRoutes"));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
-
-
